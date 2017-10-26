9736fe627656e28a0c30c41ab2c8618716ad510e
Joey Albrecht <joey@joseph-albrecht.me>
2017-10-25 23:00:40 -0400

    Added a test for hierarchical_tags.tag_expansion.expand_tag_string

--- tests/test_tag_expansion.py
+++ tests/test_tag_expansion.py
@@ -1,9 +1,15 @@
 import pytest
-from src.tag_expansion import expand_tags
+from hierarchical_tags.tag_expansion import expand_tags
+from hierarchical_tags.tag_expansion import expand_tags_string
 
 def test_expand_tags_should_return_an_iterator_with_all_sub_tags():
-    expected_sub_tags = ['sub', 'sub/tags', 'sub/tags/set']
-    actual_sub_tags = [sub for sub in expand_tags('sub/tags/set')]
+    expected_sub_tags = ['sub', 'sub/tags', 'sub/tags/list']
+    actual_sub_tags = [sub for sub in expand_tags('sub/tags/list')]
+    assert expected_sub_tags == actual_sub_tags
+
+def test_expand_tags_string_should_return_string_with_all_sub_tags():
+    expected_sub_tags = "sub sub/tags sub/tags/string"
+    actual_sub_tags = expand_tags_string("sub/tags/string")
     assert expected_sub_tags == actual_sub_tags
     
     
