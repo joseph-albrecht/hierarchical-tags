d36953b4edb28c642faaaefed98bcd75d8bd0aa9
Joey Albrecht <joey@joseph-albrecht.me>
2017-10-26 19:10:33 -0400

    Wrote a test for expand_tags with a different delimiter

--- tests/test_tag_expansion.py
+++ tests/test_tag_expansion.py
@@ -7,6 +7,11 @@ def test_expand_tags_should_return_an_iterator_with_all_sub_tags():
     actual_sub_tags = [sub for sub in expand_tags('sub/tags/list')]
     assert expected_sub_tags == actual_sub_tags
 
+def test_expand_tags_with_different_delimiter():
+    expected_sub_tags = ['sub', 'sub:tags', 'sub:tags:list']
+    actual_sub_tags = [sub for sub in expand_tags('sub:tags:list', ':')]
+    assert expected_sub_tags == actual_sub_tags
+
 def test_expand_tags_string_should_return_string_with_all_sub_tags():
     expected_sub_tags = "sub sub/tags sub/tags/string"
     actual_sub_tags = expand_tags_string("sub/tags/string")
